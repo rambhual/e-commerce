4b811fc34649f68522fdbf0950d0f159964c2253
unknown <Rampritsahani@outlook.in>
2019-09-16 18:27:25 +0530

    adding heroku

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 "# e-commerce" 
+heroku create dni-fruits-live --buildpack https://github.com/mars/create-react-app-buildpack.git
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { store, persistor } from "./redux/store";
-import "./index.css";
+import "./index.scss";
 import App from "./App";
 
 ReactDOM.render(
